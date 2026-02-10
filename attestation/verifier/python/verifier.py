d0633db053f4f0cca1454782a6381d49a41d15a8
sk13 <sk13@inf.tu-dresden.de>
2026-02-10 14:27:54 +0100

    More update on the verifier

--- attestation/verifier/python/verifier.py
+++ attestation/verifier/python/verifier.py
@@ -14,3 +14,18 @@ result =  dcap_qvl.verify(quote_bytes,collateral,now_timestamp)
 
 print(result)
 print(result.status)
+
+parsed_quote = dcap_qvl.parse_quote(quote_bytes) 
+print(parsed_quote.header.version)
+print(parsed_quote.header.attestation_key_type)
+print(parsed_quote.header.user_data)
+
+enclave_report = parsed_quote.report
+
+# Zugriff auf die wichtigsten Felder
+print(f"MRENCLAVE: {enclave_report.mr_enclave.hex()}")
+print(f"MRSIGNER:  {enclave_report.mr_signer.hex()}")
+print(f"Attributes: {enclave_report.attributes.hex()}")
+print(f"ReportData: {enclave_report.report_data.hex()}")
+
+
