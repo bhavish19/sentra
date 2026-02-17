fe0ddacd28c36ff2cae3e571e39327dc8fa879a5
sk13 <sk13@inf.tu-dresden.de>
2026-02-17 14:58:25 +0000

    Minor docker run fixes

--- demonstrator/ci/docker/scripts/entrypoint.sh
+++ demonstrator/ci/docker/scripts/entrypoint.sh
@@ -3,7 +3,8 @@ export PEBBLE_CHAIN_LENGTH=1
 export PEBBLE_VA_ALWAYS_VALID=1
 pebble -config /pebble/pebble-config.json  2>&1 &
 PEBBLE_PID=$!
-
+echo "Pebble is runing as process with PID $PEBBLE_PID"
 cd /sentra/backend
-python sentra-backend.py --host=0.0.0.0
+python sentra-backend.py --host=0.0.0.0 $@
+echo "Killing pebble"
 kill $PEBBLE_PID
