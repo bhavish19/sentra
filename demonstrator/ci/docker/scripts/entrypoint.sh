fdd2ff317e3cc680a7bb89ee11ea97cabb4219f2
sk13 <sk13@inf.tu-dresden.de>
2026-02-18 12:38:58 +0000

    Make the sentra backend docker exit more reliable

--- demonstrator/ci/docker/scripts/entrypoint.sh
+++ demonstrator/ci/docker/scripts/entrypoint.sh
@@ -6,6 +6,9 @@ pebble -config /pebble/pebble-config.json  2>&1 &
 PEBBLE_PID=$!
 echo "Pebble is runing as process with PID $PEBBLE_PID"
 cd /sentra/backend
-python sentra-backend.py --host=0.0.0.0 $@
-echo "Killing pebble"
-kill $PEBBLE_PID
+python sentra-backend.py --host=0.0.0.0 $@ &
+FLASK_PID=$!
+echo "Sentra Backend is runing as process with PID $FLASK_PID"
+trap "kill $PEBBLE_PID $FLASK_PID" 15 2
+
+wait $FLASK_PID
