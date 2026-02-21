3ef7cdc6bdf00b59e97c54cf87cf461af6c35b84
sk13 <sk13@inf.tu-dresden.de>
2026-02-21 22:12:13 +0100

    Better command line args handling

--- attestation/attester/build.rs
+++ attestation/attester/build.rs
@@ -1,6 +1,7 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-tonic_build::configure()
-        .build_server(false)  // We only need the client    
-	.compile(&["./SentraBackend-GRPC-Services.proto"],&["./"])?;
-    Ok(())
-}
+fn main() -> Result<(), Box<dyn std::error::Error>> 
+    {
+        tonic_build::configure()
+            .build_server(false)  // We only need the client    
+	        .compile(&["./SentraBackend-GRPC-Services.proto"],&["./"])?;
+        Ok(())
+    }
--- sgx/enclave_run_script.sh
+++ sgx/enclave_run_script.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+
+rust_args=()
+python_args=()
+current=1
+
+for arg in "$@"; do
+  if [ "$arg" = "---" ]; then
+    current=2
+    continue
+  fi
+  if [ "$current" -eq 1 ]; then
+    rust_args+=("$arg")
+  else
+    python_args+=("$arg")
+  fi
+done
+
 echo "Run attester in background"
-/bin/sentra_attester http://sentra-backend:8000 &
-echo "run training"
-/bin/python3 "$@" #&
+/bin/sentra_attester "${rust_args[@]}" &
+echo "Run training"
+/bin/python3 "${python_args[@]}"
