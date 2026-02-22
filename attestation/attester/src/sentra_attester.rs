a9b197faa45aa283c2c22a30709b514c2a1ae4ad
sk13 <sk13@inf.tu-dresden.de>
2026-02-22 14:51:50 +0100

    renenable quote generation

--- attestation/attester/src/sentra_attester.rs
+++ attestation/attester/src/sentra_attester.rs
@@ -1,4 +1,4 @@
-//use occlum_sgx::SGXQuote;
+use occlum_sgx::SGXQuote;
 
 pub fn generate_attestation_report() ->  Vec<u8> {
 
@@ -6,7 +6,7 @@ pub fn generate_attestation_report() ->  Vec<u8> {
    let report_data = [0u8; 64];
     // 2. Generate the SGX Quote
 
-  /*   let quote = SGXQuote::from_report_data(&report_data).unwrap();
+     let quote = SGXQuote::from_report_data(&report_data).unwrap();
     // 3. (Optional) Inspect the quote metadata
 
     println!("MrEnclave: {:?}", quote.mrenclave());
@@ -14,7 +14,6 @@ pub fn generate_attestation_report() ->  Vec<u8> {
     // 4. Convert to bytes for transmission to a remote verifier
 
     let quote = quote.as_slice();
-    return quote.to_vec();*/
-    return report_data.to_vec();
+    return quote.to_vec();
 }
 
