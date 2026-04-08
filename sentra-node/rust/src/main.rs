774a503094cf35885ff4b97b7f6d555be2d3c47d
sk13 <sk13@inf.tu-dresden.de>
2026-04-08 21:20:56 +0200

    small bug fix

--- sentra-node/rust/src/main.rs
+++ sentra-node/rust/src/main.rs
@@ -1,7 +1,7 @@
 mod sentra_attester;
 mod acme;
 
-use std::{error::Error, time::Duration};
+use std::{error::Error, time::Duration,process::Command};
 
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
