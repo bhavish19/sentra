e196b3f9cab5817fa2ad9c3fe0ba78cf0a0defa9
sk13 <sk13@inf.tu-dresden.de>
2026-04-10 17:05:21 +0200

    enhanced support for none sgx execution

--- sentra-node/docker/entrypoint-no-sgx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#/opt/occlum/start_aesm.sh
-wait-for-it sentra-backend:8888
-/bin/enclave_run_script-no-sgx.sh "$@"
--- sentra-node/docker/entrypoint.sh
+++ sentra-node/docker/entrypoint.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 #/opt/occlum/start_aesm.sh
+NO_SGX=false
+args=("$@") # Save command line args
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -no_sgx)
+            NO_SGX=true
+            shift 1
+            ;;
+        *)
+            shift 1
+            ;;
+    esac
+done
+
+set -- "${args[@]}" #Restore command line args
+
 wait-for-it -t 60 sentra-backend:8888
-occlum run /bin/enclave_run_script.sh "$@"
+
+
+if ! $NO_SGX; then
+    occlum run /bin/enclave_run_script.sh "$@"
+else
+    /bin/enclave_run_script.sh "$@"
+fi
